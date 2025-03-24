9826b172e86c7b527b8adc816b2e4daeea1f087d
iamnicky <2020ugee067@nitjsr.ac.in>
2025-03-24 23:54:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
